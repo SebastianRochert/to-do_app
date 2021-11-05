1e391f0a51869759abb4acc4e33f89836f1535ea
SebastianRochert <sebastian.rochert@t-online.de>
2021-11-05 13:48:17 +0100

    Update: Bootstrapped the application

---
+++ config/default.ts
@@ -0,0 +1,4 @@
+export default {
+    port: 1338,
+    dbUri: "mongodb://localhost:27017/to-do_app",
+}
---
+++ src/utils/connect.ts
@@ -0,0 +1,41 @@
+import {MongoClient} from "mongodb";
+import mongoose from "mongoose";
+import config from "config";
+import logger from "./logger";
+
+async function connect(){
+    const dbUri = config.get<string>("dbUri");
+
+    try {
+        await mongoose.connect(dbUri);
+        logger.info("DB Connected");
+    } catch (error) {
+        logger.error("Could not connect to DB");
+        process.exit(1);
+    }
+}
+
+export default connect;
+
+/*
+//Try with MongoDB instead of moongoose
+async function connect() {
+    const {MongoClient} = require("mongodb");
+    const dbUri = config.get<string>("dbUri");
+
+    // Connection URL
+    const client = new MongoClient(dbUri);
+
+    // Database Name
+    const dbName = "to-do_app";
+
+    try {
+        // Use connect method to connect to the server
+        await client.connect();
+        logger.info("DB Connected!");
+    } catch (error) {
+        logger.error("Could not connect to db!");
+        process.exit(1);
+    }
+}
+ */
